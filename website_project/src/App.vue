833698047b14e98d3e19d03b0b9e4d776d8a92d4
Jakobeus23 <77895616+Jakobeus23@users.noreply.github.com>
2022-02-01 00:07:43 -0600

    Initialize
    Implemented basic structure, can move submitted review to webpage, but no save method yet. Stuck on scss.

---
+++ website_project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id = "app">
+    <UserProfile/>
+    </div>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile.vue"
+
+export default {
+  name: 'App',
+  components: { UserProfile }
+}
+</script>
+
+<style>
+#app {
+ font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #b9b9b9;
+  min-height: 100vh;
+  background-color: #0d1424;
+}
+</style>
---
+++ website_project/src/components/ReviewItem.vue
@@ -0,0 +1,63 @@
+<template>
+<div class = "reviewItem" @click="favoriteReview(review.id)">
+    <div class = "userReview">
+        <div class = "userReviewItem">
+            @{{ username }}
+            <div class = "reviewContext"> 
+                Type - {{ review.type }}
+                <div>Genre - {{ review.genre }} </div>
+            </div>
+        </div>
+        <div class = "reviewItemContent">
+            {{ review.content }}
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: "ReviewItem",
+    props: {
+        username: {
+            type: String,
+            required: true
+        },
+        review: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        favoriteReview(id) {
+            this.$emit('favorite', id)
+        }
+    }
+};
+</script>
+
+<style scoped>
+.reviewItem {
+    padding: 20px;
+    background-color: #0d1424;
+    border-radius: 5px;
+    border: 1px solid #a7a7a7;
+    box-sizing: border-box;
+    cursor: pointer;
+    transition: all 0.24s ease;
+    width: 550px;
+    margin: auto;
+    margin-top: 5px;
+}
+
+.reviewItem:hover {
+    transform: scale(1.1, 1.1);
+}
+.userReviewItem {
+     font-weight: bold;
+ }
+
+.reviewContext {
+    font-weight: normal;
+}
+</style>
